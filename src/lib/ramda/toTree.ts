3a6e2a50497398e2b36b4e3ffa052324b1b7b8a7
Andreas Herd <mendrik76@gmai.com>
2024-10-09 23:16:11 +0300

    add toTree function

---
+++ src/lib/ramda/toTree.ts
@@ -0,0 +1,21 @@
+type TreeOf<T, C extends string = 'children'> = Omit<T, C> &
+	Record<C, TreeOf<T>[]>
+
+export const listToTree =
+	<T>(
+		idProp: keyof T & string,
+		parentProp: keyof T & string,
+		childProp: string
+	) =>
+	(list: T[]): TreeOf<T, typeof childProp> =>
+		list
+			.filter(item => !item[parentProp])
+			.map(function buildTree(node): any {
+				return {
+					...node,
+					[childProp]: list
+						.filter(child => child[parentProp] === node[idProp])
+						.map(buildTree)
+				}
+			})
+			.shift()
