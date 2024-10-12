3176b88cc7f0226df8bfb2b3c5737dba1e4c70b0
Andreas Herd <mendrik76@gmai.com>
2024-10-12 22:50:22 +0300

    add tabler icons

--- src/components/ui/node.tsx
+++ src/components/ui/node.tsx
@@ -1,16 +1,44 @@
+import { cn } from '@/lib/utils'
 import type { TreeNode } from '@/state/tree'
+import { IconChevronRight, IconFile, IconFolder } from '@tabler/icons-react'
+import { isNotEmpty } from 'ramda'
+import { Button } from './button'
 
 type OwnProps = {
 	node: TreeNode
+	depth: number
 }
 
-export const Node = ({ node }: OwnProps) => {
+export const Node = ({ node, depth }: OwnProps) => {
+	const hasChildren = isNotEmpty(node.nodes)
 	return (
-		<ol>
-			<li data-node_id={node.id}>{node.name}</li>
-			{node.nodes.map(child => (
-				<Node node={child} key={child.id} />
-			))}
+		<ol className={cn(`list-none m-0`)} style={{ paddingLeft: depth * 16 }}>
+			<li data-node_id={node.id}>
+				<div className="flex flex-row items-center justify-start w-full">
+					<Button
+						type="button"
+						variant="ghost"
+						className="flex-0 shrink-0 p-0 w-4 h-auto"
+					>
+						{hasChildren ? <IconChevronRight className="w-4 h-4" /> : null}
+					</Button>
+					<Button
+						type="button"
+						variant="ghost"
+						className="flex flex-row gap-1 p-0 w-full items-center justify-start h-auto"
+					>
+						{hasChildren ? (
+							<IconFolder className="w-4 h-4" />
+						) : (
+							<IconFile className="w-4 h-4" />
+						)}
+						<div>{node.name}</div>
+					</Button>
+				</div>
+				{node.nodes.map(child => (
+					<Node node={child} key={child.id} depth={depth + 1} />
+				))}
+			</li>
 		</ol>
 	)
 }
--- src/components/ui/tree.tsx
+++ src/components/ui/tree.tsx
@@ -3,16 +3,16 @@ import type { TreeNode } from '@/state/tree'
 
 type OwnProps = {
 	root: TreeNode
-	renderRoot: boolean
+	renderRoot?: boolean
 }
 
-export const Tree = ({ root, renderRoot }: OwnProps) => {
+export const Tree = ({ root, renderRoot = false }: OwnProps) => {
 	return (
 		<div className="w-full h-full pr-1">
 			{renderRoot ? (
-				<Node node={root} />
+				<Node node={root} depth={0} />
 			) : (
-				root.nodes.map(child => <Node node={child} key={child.id} />)
+				root.nodes.map(child => <Node node={child} key={child.id} depth={0} />)
 			)}
 		</div>
 	)
