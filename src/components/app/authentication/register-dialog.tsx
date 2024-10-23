3dffc618ae986109a8d76632f2e614c205743a5d
Andreas Herd <mendrik76@gmai.com>
2024-10-23 17:03:43 +0300

    prepare auth routes

---
+++ src/components/app/authentication/register-dialog.tsx
@@ -0,0 +1,67 @@
+import { Button } from '@/components/ui/button'
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle
+} from '@/components/ui/dialog'
+import { EditorType } from '@/components/ui/tree/types'
+import { asField } from '@/components/ui/zod-form/utils'
+import { ZodForm } from '@/components/ui/zod-form/zod-form'
+import { stopPropagation } from '@/lib/dom-events'
+import {} from '@/lib/match'
+import { pipeAsync } from '@/lib/pipe-async'
+import type { Fn } from '@/type-patches/functions'
+import { pipe } from 'ramda'
+import { Link } from 'react-router-dom'
+import { type TypeOf, strictObject, string } from 'zod'
+
+const registrationSchema = strictObject({
+	email: string().describe(
+		asField({
+			label: 'Email',
+			editor: EditorType.email
+		})
+	)
+})
+
+type Registration = TypeOf<typeof registrationSchema>
+
+const registerCommand: Fn<Partial<Registration>, void> = pipeAsync(console.log)
+
+export const RegistrationDialog = () => {
+	return (
+		<Dialog open={true}>
+			<DialogContent
+				onEscapeKeyDown={close}
+				onKeyDown={stopPropagation}
+				onInteractOutside={close}
+			>
+				<DialogHeader>
+					<DialogTitle>Register an account</DialogTitle>
+					<DialogDescription>
+						Sign up to Groblin and create your first project.
+					</DialogDescription>
+				</DialogHeader>
+				<ZodForm schema={registrationSchema} onSubmit={pipe(registerCommand)}>
+					<DialogFooter className="gap-2 flex flex-row items-center">
+						<div className="mr-auto">
+							Back to{' '}
+							<Link to="/" className="text-link">
+								login
+							</Link>{' '}
+							or forgot your{' '}
+							<Link to="/password" className="text-link">
+								password
+							</Link>
+							?
+						</div>
+						<Button type="submit">Send email</Button>
+					</DialogFooter>
+				</ZodForm>
+			</DialogContent>
+		</Dialog>
+	)
+}
