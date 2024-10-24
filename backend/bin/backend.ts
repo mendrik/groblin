2e5aeb3a2ed866620d50ef19b99f78a952c846e3
Andreas Herd <mendrik76@gmai.com>
2024-10-24 13:04:49 +0300

    prepare tsx runner

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -1,7 +1,6 @@
-#!/usr/bin/env node
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
-import { BackendStack } from '../lib/backend-stack'
+import { BackendStack } from '~/backend-stack'
 
 const app = new cdk.App()
 new BackendStack(app, 'BackendStack', {
--- backend/test/backend.test.ts
+++ backend/test/backend.test.ts
@@ -1,17 +1,15 @@
-import { test } from 'node:test'
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Backend from '../lib/backend-stack';
+import { describe, expect, test } from 'vitest'
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/backend-stack.ts
-test('SQS Queue Created', () => {
-	//   const app = new cdk.App();
-	//     // WHEN
-	//   const stack = new Backend.BackendStack(app, 'MyTestStack');
-	//     // THEN
-	//   const template = Template.fromStack(stack);
-	//   template.hasResourceProperties('AWS::SQS::Queue', {
-	//     VisibilityTimeout: 300
-	//   });
+describe('backend', () => {
+	test('SQS Queue Created', () => {
+		expect(1).toBe(1)
+		//   const app = new cdk.App();
+		//     // WHEN
+		//   const stack = new Backend.BackendStack(app, 'MyTestStack');
+		//     // THEN
+		//   const template = Template.fromStack(stack);
+		//   template.hasResourceProperties('AWS::SQS::Queue', {
+		//     VisibilityTimeout: 300
+		//   });
+	})
 })
