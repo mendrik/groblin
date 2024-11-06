c1bd0ec365290b2a1dc78b8d238bdd6ae9f1b6a0
Andreas Herd <mendrik76@gmai.com>
2024-11-06 14:25:43 +0200

    cont. focus travel

--- frontend/src/components/utils/focus-travel.tsx
+++ frontend/src/components/utils/focus-travel.tsx
@@ -1,10 +1,19 @@
+import { caseOf, match } from '@/lib/match'
 import { hasMethod } from '@/lib/utils'
+import {
+	always,
+	converge,
+	dec,
+	findIndex,
+	identity,
+	inc,
+	pipe,
+	sort
+} from 'ramda'
 import { type PropsWithChildren, useEffect, useRef } from 'react'
 import KeyListener from './key-listener'
 
 type OwnProps = PropsWithChildren<{
-	horizontal?: boolean
-	vertical?: boolean
 	focusableSelector?: string
 }>
 
@@ -20,24 +29,64 @@ const defaultFocusable =
 
 const focusFirst = (focusableSelector: string) => (el: HTMLElement | null) => {
 	const focusable = el?.querySelector(focusableSelector)
-	if (hasMethod(focusable, 'focus')) {
-		setTimeout(() => focusable.focus(), 40)
-	}
+	if (hasMethod(focusable, 'focus')) setTimeout(() => focusable.focus(), 40)
 }
 
+const getFocusableElements = (selector: string) => (el: HTMLElement | null) =>
+	Array.from(el?.querySelectorAll(selector) ?? []) as HTMLElement[]
+
+const sorters = {
+	[Direction.Right]: (a: DOMRect, b: DOMRect) => a.left - b.left,
+	[Direction.Left]: (a: DOMRect, b: DOMRect) => b.left - a.left,
+	[Direction.Up]: (a: DOMRect, b: DOMRect) => b.top - a.top,
+	[Direction.Down]: (a: DOMRect, b: DOMRect) => a.top - b.top
+}
+
+const isLeftOrUp = (dir: Direction) =>
+	always(dir === Direction.Left || dir === Direction.Up)
+
+const isRightOrDown = (dir: Direction) =>
+	always(dir === Direction.Right || dir === Direction.Down)
+
+const clampIndex = (idx: number, xs: HTMLElement[]) =>
+	xs[(idx + xs.length) % xs.length]
+
+const sortFocusable = (dir: Direction) =>
+	sort<HTMLElement>((a, b) =>
+		sorters[dir](a.getBoundingClientRect(), b.getBoundingClientRect())
+	)
+
+const focusNext = (dir: Direction) =>
+	pipe(
+		getFocusableElements(defaultFocusable),
+		converge(clampIndex, [
+			pipe(
+				sortFocusable(dir),
+				findIndex(e => e === document.activeElement),
+				match(caseOf([isLeftOrUp(dir)], dec), caseOf([isRightOrDown(dir)], inc))
+			),
+			identity
+		]),
+		(el: HTMLElement) => el.focus()
+	)
+
 const FocusTravel = ({
-	horizontal,
-	vertical,
 	focusableSelector = defaultFocusable,
 	children
 }: OwnProps) => {
 	const ref = useRef<HTMLDivElement>(null)
 
 	const focus = (dir: Direction) => (ev: Event) => {
-		console.log(ref.current)
+		if (ref.current) {
+			ev.preventDefault() // Prevent default scrolling behavior
+			ev.stopImmediatePropagation()
+			focusNext(dir)(ref.current)
+		}
 	}
 
-	useEffect(() => focusFirst(focusableSelector)(ref.current))
+	useEffect(() => {
+		focusFirst(focusableSelector)(ref.current)
+	}, [focusableSelector])
 
 	return (
 		<KeyListener
