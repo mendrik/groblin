de8776796ec1ffb99e1c61e3b20e389f5f77ac9c
Andreas Herd <mendrik76@gmai.com>
2024-11-13 21:40:52 +0200

    improve gql client

--- frontend/src/components/app/tags.tsx
+++ frontend/src/components/app/tags.tsx
@@ -28,9 +28,9 @@ const ActiveTab = ({ tag }: TabProps) => {
 			<SortableItem
 				key={`${tag.id}`}
 				id={tag.id}
-				className="h-7 z-10"
 				renderer={({ listeners }) => (
 					<div
+						key={`${tag.id}`}
 						className={cn(
 							'h-7 whitespace-nowrap rounded-md border border-red relative flex items-center justify-center',
 							'ring-offset-background transition-all text-foreground shadow',
@@ -80,58 +80,54 @@ const reorderCommand = ({ active, over }: DragEndEvent) => {
 	})
 }
 
-export const Tags = () => {
-	if (!$tag.value) return null
-
-	return (
-		<div className="flex flex-row gap-1 items-center">
-			<SortContext
-				strategy={horizontalListSortingStrategy}
-				onDragEnd={reorderCommand}
-				values={$tags.value}
-			>
-				<Tabs
-					value={`${notNil($tag).id}`}
-					className="w-fit"
-					onValueChange={selectTag}
+export const Tags = () => (
+	<div className="flex flex-row gap-1 items-center">
+		<Tabs
+			value={`${notNil($tag).id}`}
+			className="w-fit"
+			onValueChange={selectTag}
+		>
+			<TabsList className="h-8 select-none" id="tags">
+				<SortContext
+					strategy={horizontalListSortingStrategy}
+					onDragEnd={reorderCommand}
+					values={$tags.value}
 				>
-					<TabsList className="h-8 select-none" id="tags">
-						{$tags.value.map(tag =>
-							tag.id === notNil($tag).id ? (
-								<ActiveTab tag={tag} key={tag.id} />
-							) : (
-								<SortableItem
-									key={`${tag.id}`}
-									id={tag.id}
-									className="h-7"
-									renderer={({ listeners, setActivatorNodeRef }) => (
-										<TabsTrigger
-											key={tag.id}
-											value={`${tag.id}`}
-											className="p-0"
-											ref={setActivatorNodeRef}
-											{...listeners}
-										>
-											<TagName tag={tag} />
-										</TabsTrigger>
-									)}
-								/>
-							)
-						)}
-					</TabsList>
-				</Tabs>
-			</SortContext>
-			<IconButton
-				icon={IconTag}
-				size="sm"
-				variant="ghost"
-				onClick={openTagCreate}
-			>
-				new tag
-			</IconButton>
-			<TagCreate />
-			<TagEdit />
-			<TagDelete />
-		</div>
-	)
-}
+					{$tags.value.map(tag =>
+						tag.id === notNil($tag).id ? (
+							<ActiveTab tag={tag} key={`${tag.id}`} />
+						) : (
+							<SortableItem
+								key={`${tag.id}`}
+								id={tag.id}
+								className="h-7"
+								renderer={({ listeners, setActivatorNodeRef }) => (
+									<TabsTrigger
+										key={`${tag.id}`}
+										value={`${tag.id}`}
+										className="p-0"
+										ref={setActivatorNodeRef}
+										{...listeners}
+									>
+										<TagName tag={tag} />
+									</TabsTrigger>
+								)}
+							/>
+						)
+					)}
+				</SortContext>
+			</TabsList>
+		</Tabs>
+		<IconButton
+			icon={IconTag}
+			size="sm"
+			variant="ghost"
+			onClick={openTagCreate}
+		>
+			new tag
+		</IconButton>
+		<TagCreate />
+		<TagEdit />
+		<TagDelete />
+	</div>
+)
--- frontend/src/gql-client.ts
+++ frontend/src/gql-client.ts
@@ -13,29 +13,10 @@ const gql = createClient({
 export const query = async <D extends TypedDocumentString<any, any>>(
 	queryDoc: D,
 	variables?: VariablesOf<D>
-): Promise<ResultOf<D>> =>
-	new Promise((res, rej) => {
-		const unsub = gql.subscribe(
-			{ query: queryDoc.toString(), variables },
-			{
-				next: ({ data, errors }) => {
-					unsub()
-					if (errors) {
-						console.error(...errors)
-						rej(Object.assign(new Error(), errors[0]))
-					} else {
-						res(data as ResultOf<D>)
-					}
-				},
-				error: err => {
-					unsub()
-					console.error(err)
-					rej(Object.assign(new Error(), err))
-				},
-				complete: () => unsub()
-			}
-		)
-	})
+): Promise<ResultOf<D>> => {
+	const query = gql.iterate({ query: queryDoc.toString(), variables })
+	return query.next().then(({ value }) => value.data as ResultOf<D>)
+}
 
 export const subscribe = async <S extends TypedDocumentString<any, any>>(
 	subscriptionDoc: S,
