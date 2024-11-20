a6fb7b1687f1cdf65d3c1869fa8a18354e212162
Andreas Herd <mendrik76@gmai.com>
2024-11-20 20:17:21 +0200

    list editor

--- frontend/src/components/ui/values/list-editor.tsx
+++ frontend/src/components/ui/values/list-editor.tsx
@@ -1,9 +1,29 @@
 import { notNil } from '@/lib/utils'
 import type { TreeNode } from '@/state/tree'
 import { $valueMap } from '@/state/value'
-import { IconSquareRoundedPlus } from '@tabler/icons-react'
+import {
+	type Icon,
+	IconSquareRoundedMinus,
+	IconSquareRoundedPlus
+} from '@tabler/icons-react'
 import { Button } from '../button'
 import { openListItemCreate } from './list-item-create'
+
+type ListButtonProps = {
+	onClick: () => void
+	icon: Icon
+}
+
+const ListButton = ({ onClick, icon: Icon }: ListButtonProps) => (
+	<Button
+		size="icon"
+		variant="ghost"
+		className="p-1 h-5 w-5 content-center"
+		onClick={onClick}
+	>
+		<Icon className="h-4 w-4 shrink-0 text-muted-foreground" stroke={1} />
+	</Button>
+)
 
 type OwnProps = {
 	node: TreeNode
@@ -12,7 +32,7 @@ type OwnProps = {
 export const ListEditor = ({ node }: OwnProps) => {
 	const items = notNil($valueMap)[node.id] ?? []
 	return (
-		<div className="flex flex-row w-full h-7">
+		<div className="flex flex-row w-full h-7 gap-0 items-center">
 			<ol className="flex flex-row items-center -ml-1">
 				{items.map(item => (
 					<Button
@@ -25,17 +45,11 @@ export const ListEditor = ({ node }: OwnProps) => {
 					</Button>
 				))}
 			</ol>
-			<Button
-				size="icon"
-				variant="ghost"
-				className="p-1 h-7 w-7 content-center"
+			<ListButton
+				icon={IconSquareRoundedPlus}
 				onClick={() => openListItemCreate(node)}
-			>
-				<IconSquareRoundedPlus
-					className="h-5 w-5 shrink-0 text-muted-foreground"
-					stroke={1}
-				/>
-			</Button>
+			/>
+			<ListButton icon={IconSquareRoundedMinus} onClick={() => void 0} />
 		</div>
 	)
 }
