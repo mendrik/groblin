b480521f029fbc0a8d5d2c95d23492c5eaa999e4
Andreas Herd <mendrik76@gmai.com>
2024-11-22 22:02:31 +0200

    prepare blanks

--- frontend/src/components/app/node-values.tsx
+++ frontend/src/components/app/node-values.tsx
@@ -1,4 +1,5 @@
 import { $openNodes } from '@/state/tree'
+import { $activeItems } from '@/state/value'
 import { ListItemCreate } from '../ui/values/list-item-create'
 import { ListItemDelete } from '../ui/values/list-item-delete'
 import { ValueEditor } from '../ui/values/value-editor'
@@ -9,7 +10,7 @@ export const NodeValues = () => {
 			<ol className="flex flex-col text-sm px-2 grid-lines">
 				{$openNodes.value.map(node => (
 					<li key={node.id} className="h-7 flex flex-row items-center">
-						<ValueEditor node={node} />
+						<ValueEditor node={node} active={$activeItems.value} />
 					</li>
 				))}
 			</ol>
