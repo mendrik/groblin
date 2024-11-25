ee862bd5423dbd134ddca9cae76d3f7543af1242
Andreas Herd <mendrik76@gmai.com>
2024-11-25 15:08:20 +0200

    add focus travel

--- frontend/src/components/app/node-values.tsx
+++ frontend/src/components/app/node-values.tsx
@@ -3,17 +3,20 @@ import { $valueMap } from '@/state/value'
 import { ListItemCreate } from '../ui/values/list-item-create'
 import { ListItemDelete } from '../ui/values/list-item-delete'
 import { ValueEditor } from '../ui/values/value-editor'
+import FocusTravel from '../utils/focus-travel'
 
 export const NodeValues = () => {
 	return (
 		<>
-			<ol className="flex flex-col text-sm px-2 grid-lines">
-				{$openNodes.value.map(node => (
-					<li key={node.id} className="h-7 flex flex-row items-center">
-						<ValueEditor node={node} value={$valueMap.value[node.id]} />
-					</li>
-				))}
-			</ol>
+			<FocusTravel>
+				<ol className="flex flex-col text-sm px-2 grid-lines">
+					{$openNodes.value.map(node => (
+						<li key={node.id} className="h-7 flex flex-row items-center">
+							<ValueEditor node={node} value={$valueMap.value[node.id]} />
+						</li>
+					))}
+				</ol>
+			</FocusTravel>
 			<ListItemCreate />
 			<ListItemDelete />
 		</>
