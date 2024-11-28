0321d5e6b448dd6a11f0c9223eacbf4116870f42
Andreas Herd <mendrik76@gmai.com>
2024-11-28 15:13:09 +0200

    add icons

--- frontend/src/components/ui/tree/node-icon.tsx
+++ frontend/src/components/ui/tree/node-icon.tsx
@@ -2,14 +2,22 @@ import { NodeType } from '@/gql/graphql'
 import { caseOf, match } from '@/lib/match'
 import type { TreeNode } from '@/state/tree'
 import {
-	IconBox,
-	IconCalendar,
+	IconCalendarClock,
+	IconCalendarMonth,
+	IconClock,
 	IconDatabase,
+	IconHelpHexagon,
 	IconLetterCase,
+	IconMail,
+	IconMapPin,
 	IconNumber123,
+	IconPalette,
+	IconPaperclip,
+	IconPhoneCall,
 	type IconProps,
 	IconSitemap,
-	IconToggleLeft
+	IconToggleLeft,
+	IconWorldWww
 } from '@tabler/icons-react'
 import { T as _ } from 'ramda'
 import type { ReactNode } from 'react'
@@ -26,9 +34,21 @@ export const NodeIcon = ({ node, ...props }: OwnProps) =>
 		caseOf([{ type: NodeType.Boolean }, _], (_, p) => (
 			<IconToggleLeft {...p} />
 		)),
-		caseOf([{ type: NodeType.Date }, _], (_, p) => <IconCalendar {...p} />),
+		caseOf([{ type: NodeType.Date }, _], (_, p) => (
+			<IconCalendarMonth {...p} />
+		)),
 		caseOf([{ type: NodeType.List }, _], (_, p) => <IconDatabase {...p} />),
 		caseOf([{ type: NodeType.Number }, _], (_, p) => <IconNumber123 {...p} />),
 		caseOf([{ type: NodeType.String }, _], (_, p) => <IconLetterCase {...p} />),
-		caseOf([{ type: NodeType.Schema }, _], (_, p) => <IconBox {...p} />)
+		caseOf([{ type: NodeType.Color }, _], (_, p) => <IconPalette {...p} />),
+		caseOf([{ type: NodeType.Location }, _], (_, p) => <IconMapPin {...p} />),
+		caseOf([{ type: NodeType.Email }, _], (_, p) => <IconMail {...p} />),
+		caseOf([{ type: NodeType.Media }, _], (_, p) => <IconPaperclip {...p} />),
+		caseOf([{ type: NodeType.Phone }, _], (_, p) => <IconPhoneCall {...p} />),
+		caseOf([{ type: NodeType.Time }, _], (_, p) => <IconClock {...p} />),
+		caseOf([{ type: NodeType.Url }, _], (_, p) => <IconWorldWww {...p} />),
+		caseOf([{ type: NodeType.Event }, _], (_, p) => (
+			<IconCalendarClock {...p} />
+		)),
+		caseOf([_, _], (_, p) => <IconHelpHexagon {...p} />)
 	)(node, props)
