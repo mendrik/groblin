ab404b2723f0c0e725bd941402a2a5b4ceb3725d
Andreas Herd <mendrik76@gmai.com>
2024-12-05 22:51:45 +0200

    prepare import menu

--- frontend/src/components/ui/tree/node-extra-actions.tsx
+++ frontend/src/components/ui/tree/node-extra-actions.tsx
@@ -2,7 +2,7 @@ import { type NodeSettings, NodeType } from '@/gql/graphql'
 import { caseOf, match } from '@/lib/match'
 import { $nodeSettingsMap } from '@/state/node-settings'
 import type { TreeNode } from '@/state/tree'
-import { IconPackageImport } from '@tabler/icons-react'
+import { IconPackageImport, IconTrash } from '@tabler/icons-react'
 import { T as _, isEmpty } from 'ramda'
 import type { ReactNode } from 'react'
 import { DropdownMenuItem } from '../dropdown-menu'
@@ -13,13 +13,22 @@ type OwnProps = {
 }
 
 const ImportArray = ({ node }: OwnProps) => (
-	<DropdownMenuItem
-		className="flex gap-2 items-center"
-		onSelect={() => openImportJson(node)}
-	>
-		<IconPackageImport className="w-4 h-4" />
-		<span>Import...</span>
-	</DropdownMenuItem>
+	<>
+		<DropdownMenuItem
+			className="flex gap-2 items-center"
+			onSelect={() => openImportJson(node)}
+		>
+			<IconPackageImport className="w-4 h-4" />
+			<span>Import...</span>
+		</DropdownMenuItem>
+		<DropdownMenuItem
+			className="flex gap-2 items-center"
+			onSelect={() => console.log('truncate')}
+		>
+			<IconTrash className="w-4 h-4" />
+			<span>Truncate...</span>
+		</DropdownMenuItem>
+	</>
 )
 
 const ImportObject = ({ node }: OwnProps) => (
