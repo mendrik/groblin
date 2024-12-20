08ac4cda437a6b6113e3d0578cbe4bb09ef4ec75
Andreas Herd <mendrik76@gmai.com>
2024-12-20 15:03:43 +0200

    add alert

--- frontend/src/components/app/preview-panel.tsx
+++ frontend/src/components/app/preview-panel.tsx
@@ -2,20 +2,31 @@ import type { NodeSettings } from '@/gql/graphql'
 import { $nodeSettingsMap } from '@/state/node-settings'
 import { $focusedNode, type TreeNode, asNode } from '@/state/tree'
 import { caseOf, match } from '@shared/utils/match'
+import { IconAlertCircle } from '@tabler/icons-react'
 import { T as _ } from 'ramda'
 import type { ReactNode } from 'react'
+import { Alert, AlertDescription, AlertTitle } from '../ui/alert'
 
 const matcher = match<[TreeNode, NodeSettings | undefined], ReactNode>(
 	caseOf([_, _], 'preview panel')
 )
 
+const Warning = () => (
+	<Alert variant="default" className="max-w-sm m-auto">
+		<IconAlertCircle className="h-4 w-4" />
+		<AlertTitle>Notice</AlertTitle>
+		<AlertDescription>
+			To activate the preview panel, select a node in the document tree.
+		</AlertDescription>
+	</Alert>
+)
+
 export const PreviewPanel = () => {
 	const focusedNode = $focusedNode.value
-	if (!focusedNode) return <div>Select a node</div>
-	const settings = $nodeSettingsMap.value[focusedNode]
+	const settings = $nodeSettingsMap.value[focusedNode ?? 0]
 	return (
 		<div className="flex-1 min-h-svh p-2">
-			{matcher(asNode(focusedNode), settings)}
+			{focusedNode ? matcher(asNode(focusedNode), settings) : <Warning />}
 		</div>
 	)
 }
