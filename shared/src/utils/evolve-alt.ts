1f1b3e0bc03233f884b35329877a8a6e35449924
Andreas Herd <mendrik76@gmai.com>
2024-12-30 18:03:04 +0200

    remove cast

--- shared/src/utils/evolve-alt.ts
+++ shared/src/utils/evolve-alt.ts
@@ -38,12 +38,8 @@ export function evolveAlt(specs: any, source?: any): any {
 		return (obj: any) => evolveAlt(specs, obj)
 	} else {
 		const fork = match<[any, any], any>(
-			caseOf([isArray, isPlainObj], (prop, spec) =>
-				map(evolveAlt(spec as any), prop)
-			),
-			caseOf([isPlainObj, isPlainObj], (obj, spec) =>
-				evolveAlt(spec as any, obj)
-			),
+			caseOf([isArray, isPlainObj], (prop, spec) => map(evolveAlt(spec), prop)),
+			caseOf([isPlainObj, isPlainObj], (obj, spec) => evolveAlt(spec, obj)),
 			caseOf([isNotUndefined, isFunction], (prop, fn) => fn(prop)),
 			caseOf([isUndefined, isFunction], (_, fn) => fn(source)),
 			caseOf([T, T], (prop, _) => prop)
