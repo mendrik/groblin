704fd6b42eb0452b6f82194ff770f4b8bc96917e
Andreas Herd <mendrik76@gmai.com>
2025-01-02 12:34:10 +0200

    improve micro clicks

--- frontend/src/components/ui/random/micro-icon.tsx
+++ frontend/src/components/ui/random/micro-icon.tsx
@@ -1,16 +1,23 @@
 import type { Icon, IconProps } from '@tabler/icons-react'
-import { Button } from '../button'
+import { Button, type ButtonProps } from '../button'
 
 type ListButtonProps = {
 	icon: Icon
-} & IconProps
+	onClick?: ButtonProps['onClick']
+} & Omit<IconProps, 'onClick'>
 
 export const MicroIcon = ({
 	icon: Icon,
 	stroke = 2,
+	onClick,
 	...rest
 }: ListButtonProps) => (
-	<Button size="icon" variant="ghost" className="p-1 h-5 w-5 content-center">
+	<Button
+		size="icon"
+		variant="ghost"
+		className="p-1 h-5 w-5 content-center"
+		onClick={onClick}
+	>
 		<Icon
 			className="h-4 w-4 shrink-0 text-muted-foreground"
 			stroke={stroke}
