a8ff1720ae7129c4da4781a01965af8f4c37894a
Andreas Herd <mendrik76@gmai.com>
2025-02-20 12:48:44 +0200

    feat: update number hashing utility to use PBKDF2 for key derivation; enhance encryption and decryption functions

--- shared/src/utils/number-hash.ts
+++ shared/src/utils/number-hash.ts
@@ -1,19 +1,22 @@
-import {randomBytes, createCipheriv, createDecipheriv} from 'node:crypto'
+import { createCipheriv, createDecipheriv, pbkdf2Sync } from 'node:crypto'
 
-// Encryption key and IV (Initialization Vector)
-const key = randomBytes(32); // 256-bit key
-const iv = randomBytes(16);  // 128-bit IV
+const staticString = 'my-secret-string'
+const salt = '3456salt'
+
+const derivedKey = pbkdf2Sync(staticString, salt, 100000, 48, 'sha256')
+const key = derivedKey.subarray(0, 32) // First 32 bytes for the key
+const iv = derivedKey.subarray(32, 48)
 
 export function encryptInteger(integer: number) {
-    const cipher = createCipheriv('aes-256-cbc', key, iv);
-    let encrypted = cipher.update(integer.toString(), 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
+	const cipher = createCipheriv('aes-256-cbc', key, iv)
+	let encrypted = cipher.update(integer.toString(), 'utf8', 'hex')
+	encrypted += cipher.final('hex')
+	return encrypted
 }
 
 export function decryptInteger(encrypted: string) {
-    const decipher = createDecipheriv('aes-256-cbc', key, iv);
-    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return Number.parseInt(decrypted);
+	const decipher = createDecipheriv('aes-256-cbc', key, iv)
+	let decrypted = decipher.update(encrypted, 'hex', 'utf8')
+	decrypted += decipher.final('utf8')
+	return Number.parseInt(decrypted)
 }
