37196d8ef60b4e9312b9222aa9f778db8b8eaa2c
Andreas Herd <mendrik76@gmai.com>
2025-03-04 22:41:03 +0200

    feat: update dependencies and add better-auth integration for authentication

---
+++ backend/src/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { PostgresDialect } from "kysely";
+import pg from 'pg'
+
+const dialect = new PostgresDialect({
+    pool: new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        max: 10
+    })
+})
+
+export const auth = betterAuth({
+    database: {
+        dialect,
+        type: 'postgres'
+    }
+})
